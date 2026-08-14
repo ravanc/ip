989f5d935142d0100c48cbbcceead317f9901c0c
Ravan <ravan2003@gmail.com>
2026-08-14 16:39:37 +0800

    [feat] changed name of bot from Duke to Shannon
    Shannon because Claude Shannon

---
+++ src/main/java/Shannon.java
@@ -0,0 +1,18 @@
+/**
+ * Entry point of the Shannon chatbot.
+ * <p>
+ * The class name must match the file name ({@code Shannon.java}) because the
+ * class is {@code public} - this is a Java requirement, not just a convention.
+ */
+public class Shannon {
+    public static void main(String[] args) {
+        String banner = "   oo_    \\\\  //       \\\\\\  ///\\\\\\  ///   .-.   \\\\\\  ///\n"
+                + "  /  _)-< (o)(o)   /)  ((O)(O))((O)(O)) c(O_O)c ((O)(O))\n"
+                + "  \\__ `.  ||  || (o)(O) | \\ ||  | \\ || ,'.---.`, | \\ ||\n"
+                + "     `. | |(__)|  //\\\\  ||\\\\||  ||\\\\||/ /|_|_|\\ \\||\\\\||\n"
+                + "     _| | /.--.\\ |(__)| || \\ |  || \\ || \\_____/ ||| \\ |\n"
+                + "  ,-'   |-'    `-/,-. | ||  ||  ||  ||'. `---' .`||  ||\n"
+                + " (_..--'        -'   ''(_/  \\_)(_/  \\_) `-...-' (_/  \\_)\n";
+        System.out.println(banner);
+    }
+}
